984ab221285554af41f70404ba76b1739786688b
alilholt <addison.lilholt@gmail.com>
2019-08-03 20:46:45 -0400

    commit

---
+++ website-challenge-2019/README.markdown
@@ -0,0 +1,4 @@
+# Website Challenge 2018
+ _A Pen created at CodePen.io. Original URL: [https://codepen.io/alilholt/pen/qePQzB](https://codepen.io/alilholt/pen/qePQzB).
+
+ 
---
+++ website-challenge-2019/dist/script.js
@@ -0,0 +1,42 @@
+//I wouldn't mess with the JavaScript if I were you.
+
+function openPage(pageName,elmnt,color) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablink");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].style.backgroundColor = "";
+    }
+    document.getElementById(pageName).style.display = "block";
+    elmnt.style.backgroundColor = color;
+
+}
+
+document.getElementById("defaultOpen").click();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//Where is the bonus challenge?
